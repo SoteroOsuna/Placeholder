577fbc4af7ccad7df864b0485200f76b6c4785b8
SoteroOsuna <a01251505@itesm.mx>
2021-10-06 13:30:55 -0700

    Nuevo Projecto de Prueba

---
+++ PruebaTrabajoParalelo/PruebaTrabajoParalelo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PruebaTrabajoParalelo
+//
+//  Created by Sotero Osuna on 06/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
